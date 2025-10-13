c4f8b963001c8ff0197cdad9128535e941658b25
Galya777 <galdodova@abv.bg>
2025-10-13 18:20:45 +0300

    Initial commit

---
+++ Chapter11/modules.py
@@ -0,0 +1,21 @@
+
+
+from math import pi
+import sys
+import random as rdm
+from enum import Enum
+import kanzas
+from rsp7 import rock_paper_scissors
+
+print(pi)
+
+# for item in dir(rdm):
+#     print(item)
+
+print(kanzas.capital)
+kanzas.randomfunfact()
+
+print(__name__)
+print(kanzas.__name__)
+
+rock_paper_scissors()
